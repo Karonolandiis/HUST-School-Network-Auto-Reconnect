12076d9df1b051765e89fe9a25c01daa29a68805
qyqsdtc <qyqstc@gmail.com>
2023-04-19 15:53:15 +0800

    perfect

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Fix "认证设备超时，请稍后重试"
 
 This repo is forked from [auto-connect-school-network](https://github.com/Kingdo777/auto-connect-school-network), and fixed the problem "认证设备超时，请稍后重试", detailed intros please refer to the original repo.
+
+# 抓包
+
+参照下图自行抓包然后填到代码相应位置。
+
+1. 进入校园网登陆界面
+
+2. 按 F12 打开控制台，在网络选项下 ☑️ 保存日志
+
+![校园网登陆界面](./img/CleanShot-3QDdfA9k@2x.png)
+
+3. 点击登陆，然后查看 InterFace.do?method=login 对应的 headers 和 data
+
+![控制台](./img/CleanShot-LOEqLuGy@2x.png)
+
+![InterFace](./img/CleanShot-y9YxTDpp@2x.png)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #!/opt/local/bin/python
+"""
+Author: Yiqian Qian
+Description: file content
+Date: 2023-04-19 15:48:49
+LastEditors: Yiqian Qian
+LastEditTime: 2023-04-19 15:51:03
+FilePath: /undefined/Users/yiqianqian/Library/Mobile Documents/com~apple~CloudDocs/Development/HUST-School-Network-Auto-Reconnect/main.py
+"""
 
 import json
 import os
@@ -8,14 +16,14 @@ import requests
 
 
 def log(info):
-    log_file = os.environ.get('AUTO_NET_RECONNECT_LOG_FILE', "connect.log")
+    log_file = os.environ.get("AUTO_NET_RECONNECT_LOG_FILE", "connect.log")
     print(info.strip())
     with open(log_file, "a") as f:
         f.write(info.strip() + "\n")
 
 
 def login():
-    url = 'http://192.168.50.3:8080/eportal/InterFace.do?method=login'
+    url = "http://192.168.50.3:8080/eportal/InterFace.do?method=login"
     # config_file = os.environ.get('AUTO_NET_RECONNECT_CONFIG_FILE', "content")
     # with open(config_file, "r") as f:
     #     data = f.read().strip('"').strip("'")
@@ -24,47 +32,47 @@ def login():
     # }
 
     # 以下信息需要自己抓包
-    #<------将抓包得到的headers内容填写到键值对中------>
-    headers={
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-        'Cache-Control':'no-cache',
-        'Connection':'keep-alive',
-        'Content-Length':'', #填写抓包得到的值
-        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie': '', #填写抓包得到的cookie
-        'Host':'', #填写抓包得到的host
-        'Origin':'', #填写抓包得到的origin
-        'Pragma':'no-cache',
-        'Referer':'' #填写抓包得到的referer
+    # <------将抓包得到的headers内容填写到键值对中------>
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Content-Length": "",  # 填写抓包得到的值
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Cookie": "",  # 填写抓包得到的cookie
+        "Host": "",  # 填写抓包得到的host
+        "Origin": "",  # 填写抓包得到的origin
+        "Pragma": "no-cache",
+        "Referer": "",  # 填写抓包得到的referer
     }
-    #<------headers结束------>
- 
-    #<------将抓包得到的载荷内容填写到键值对中------>
-    data={
-        'userId':'', #填写抓包得到的userId
-        'password':'', #填写抓包得到的password
-        'service':'', #填写抓包得到的service
-        'queryString':'',
-        'operatorPwd':'',
-        'operatorUserId':'',
-        'validcode':'',
-        'passwordEncrypt':'false' #填写抓包得到的passwordEncrypt
+    # <------headers结束------>
+
+    # <------将抓包得到的载荷内容填写到键值对中------>
+    data = {
+        "userId": "",  # 填写抓包得到的userId
+        "password": "",  # 填写抓包得到的password
+        "service": "",  # 填写抓包得到的service
+        "queryString": "",
+        "operatorPwd": "",
+        "operatorUserId": "",
+        "validcode": "",
+        "passwordEncrypt": "false",  # 填写抓包得到的passwordEncrypt
     }
-    #<------载荷结束------>
+    # <------载荷结束------>
 
     try:
-        response = requests.post(url, data, headers=header, timeout=10)
+        response = requests.post(url, data, headers=headers, timeout=10)
         content = json.loads(response.text)
         encoding = response.encoding
-        if content['result'] == 'fail':
-            msg = content['message'].encode(encoding).decode('utf-8')
+        if content["result"] == "fail":
+            msg = content["message"].encode(encoding).decode("utf-8")
             log(msg)
             if msg == "认证设备响应超时,请稍后再试!":
                 time.sleep(120)
-            if msg == '正常上网时段为:日常06:00-23:59，请在以上时段内进行认证上网!':
+            if msg == "正常上网时段为:日常06:00-23:59，请在以上时段内进行认证上网!":
                 time.sleep(1200)
         else:
             log("login at --> " + time.asctime(time.localtime(time.time())))
@@ -86,7 +94,7 @@ def pong():
     return ping("202.114.0.242", 4) or ping("8.8.8.8", 4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
         if pong():
             time.sleep(5)
